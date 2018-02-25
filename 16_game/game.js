3f8551cadc09dab70e931cb53a56b2df19309988
Serhii Nykyforov <04mailbox@gmail.com>
2018-02-25 17:03:29 +0200

    Add `A monster` for chapter16

--- 16_game/game.js
+++ 16_game/game.js
@@ -1,4 +1,11 @@
-let simpleLevelPlan = `
+const WOBBLE_SPEED = 8, WOBBLE_DIST = 0.07;
+const PLAYER_X_SPEED = 7;
+const GRAVITY = 30;
+const JUMP_SPEED = 17;
+const SCALE = 20;
+const INIT_LIVES = 3;
+
+const simpleLevelPlan = `
 ......................
 ..#................#..
 ..#..............=.#..
@@ -9,116 +16,261 @@ let simpleLevelPlan = `
 ......##############..
 ......................`;
 
-let Level = class Level {
-  constructor(plan) {
-    let rows = plan.trim().split("\n").map(l => [...l]);
-    this.height = rows.length;
-    this.width = rows[0].length;
-    this.startActors = [];
-
-    this.rows = rows.map((row, y) => {
-      return row.map((ch, x) => {
-        let type = levelChars[ch];
-        if (typeof type === "string") return type;
-        this.startActors.push(
-          type.create(new Vec(x, y), ch));
-        return "empty";
-      });
-    });
-  }
-};
-
-let State = class State {
-  constructor(level, actors, status) {
-    this.level = level;
-    this.actors = actors;
-    this.status = status;
-  }
-
-  static start(level) {
-    return new State(level, level.startActors, "playing");
-  }
-
-  get player() {
-    return this.actors.find(a => a.type === "player");
-  }
-};
 
 let Vec = class Vec {
-  constructor(x, y) {
-    this.x = x; this.y = y;
-  }
-  plus(other) {
-    return new Vec(this.x + other.x, this.y + other.y);
-  }
-  times(factor) {
-    return new Vec(this.x * factor, this.y * factor);
-  }
+    constructor(x, y) {
+        this.x = x; this.y = y;
+    }
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
 };
+
+function overlap(actor1, actor2) {
+    return actor1.pos.x + actor1.size.x > actor2.pos.x &&
+        actor1.pos.x < actor2.pos.x + actor2.size.x &&
+        actor1.pos.y + actor1.size.y > actor2.pos.y &&
+        actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
+
+let Level = class Level {
+    constructor(plan) {
+        let rows = plan.trim().split("\n").map(l => [...l]);
+        this.height = rows.length;
+        this.width = rows[0].length;
+        this.startActors = [];
+
+        this.rows = rows.map((row, y) => {
+            return row.map((ch, x) => {
+                let type = levelChars[ch];
+                if (typeof type === "string") return type;
+                this.startActors.push(
+                    type.create(new Vec(x, y), ch));
+                return "empty";
+            });
+        });
+    }
+
+    touches(pos, size, type) {
+        let xStart = Math.floor(pos.x);
+        let xEnd = Math.ceil(pos.x + size.x);
+        let yStart = Math.floor(pos.y);
+        let yEnd = Math.ceil(pos.y + size.y);
+
+        for (let y = yStart; y < yEnd; y++) {
+            for (let x = xStart; x < xEnd; x++) {
+                let isOutside = x < 0 || x >= this.width ||
+                    y < 0 || y >= this.height;
+                let here = isOutside ? "wall" : this.rows[y][x];
+                if (here === type) return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+let State = class State {
+    constructor(level, actors, status) {
+        this.level = level;
+        this.actors = actors;
+        this.status = status;
+    }
+
+    static start(level) {
+        return new State(level, level.startActors, "playing");
+    }
+
+    get player() {
+        return this.actors.find(a => a.type === "player");
+    }
+
+    update(time, keys) {
+        let actors = this.actors
+            .map(actor => actor.update(time, this, keys));
+        let newState = new State(this.level, actors, this.status);
+
+        if (newState.status !== "playing") return newState;
+
+        let player = newState.player;
+        if (this.level.touches(player.pos, player.size, "lava")) {
+            return new State(this.level, actors, "lost");
+        }
+
+        for (let actor of actors) {
+            if (actor !== player && overlap(actor, player)) {
+                newState = actor.collide(newState);
+            }
+        }
+        return newState;
+    }
+};
+
 
 let Player = class Player {
-  constructor(pos, speed) {
-    this.pos = pos;
-    this.speed = speed;
-  }
+    constructor(pos, speed) {
+        this.pos = pos;
+        this.speed = speed;
+    }
 
-  get type() { return "player"; }
+    get type() {
+        return "player";
+    }
 
-  static create(pos) {
-    return new Player(pos.plus(new Vec(0, -0.5)),
-                      new Vec(0, 0));
-  }
+    static create(pos) {
+        return new Player(pos.plus(new Vec(0, -0.5)),
+            new Vec(0, 0));
+    }
+
+    update(time, state, keys) {
+        let xSpeed = 0;
+        if (keys.ArrowLeft) xSpeed -= PLAYER_X_SPEED;
+        if (keys.ArrowRight) xSpeed += PLAYER_X_SPEED;
+        let pos = this.pos;
+        let movedX = pos.plus(new Vec(xSpeed * time, 0));
+        if (!state.level.touches(movedX, this.size, "wall")) {
+            pos = movedX;
+        }
+
+        let ySpeed = this.speed.y + time * GRAVITY;
+        let movedY = pos.plus(new Vec(0, ySpeed * time));
+        if (!state.level.touches(movedY, this.size, "wall")) {
+            pos = movedY;
+        } else if (keys.ArrowUp && ySpeed > 0) {
+            ySpeed = -JUMP_SPEED;
+        } else {
+            ySpeed = 0;
+        }
+        return new Player(pos, new Vec(xSpeed, ySpeed));
+    }
 };
-
 Player.prototype.size = new Vec(0.8, 1.5);
 
+
 let Lava = class Lava {
-  constructor(pos, speed, reset) {
-    this.pos = pos;
-    this.speed = speed;
-    this.reset = reset;
-  }
-
-  get type() { return "lava"; }
-
-  static create(pos, ch) {
-    if (ch === "=") {
-      return new Lava(pos, new Vec(2, 0));
-    } else if (ch === "|") {
-      return new Lava(pos, new Vec(0, 2));
-    } else if (ch === "v") {
-      return new Lava(pos, new Vec(0, 3), pos);
+    constructor(pos, speed, reset) {
+        this.pos = pos;
+        this.speed = speed;
+        this.reset = reset;
     }
-  }
-};
 
+    get type() {
+        return "lava";
+    }
+
+    static create(pos, ch) {
+        if (ch === "=") {
+            return new Lava(pos, new Vec(2, 0));
+        } else if (ch === "|") {
+            return new Lava(pos, new Vec(0, 2));
+        } else if (ch === "v") {
+            return new Lava(pos, new Vec(0, 3), pos);
+        }
+    }
+
+    collide(state) {
+        return new State(state.level, state.actors, "lost");
+    }
+
+    update(time, state) {
+        let newPos = this.pos.plus(this.speed.times(time));
+        if (!state.level.touches(newPos, this.size, "wall")) {
+            return new Lava(newPos, this.speed, this.reset);
+        } else if (this.reset) {
+            return new Lava(this.reset, this.speed, this.reset);
+        } else {
+            return new Lava(this.pos, this.speed.times(-1));
+        }
+    }
+};
 Lava.prototype.size = new Vec(1, 1);
 
+
 let Coin = class Coin {
-  constructor(pos, basePos, wobble) {
-    this.pos = pos;
-    this.basePos = basePos;
-    this.wobble = wobble;
-  }
+    constructor(pos, basePos, wobble) {
+        this.pos = pos;
+        this.basePos = basePos;
+        this.wobble = wobble;
+    }
 
-  get type() { return "coin"; }
+    get type() {
+        return "coin";
+    }
 
-  static create(pos) {
-    let basePos = pos.plus(new Vec(0.2, 0.1));
-    return new Coin(basePos, basePos,
-                    Math.random() * Math.PI * 2);
-  }
+    static create(pos) {
+        let basePos = pos.plus(new Vec(0.2, 0.1));
+        return new Coin(basePos, basePos,
+            Math.random() * Math.PI * 2);
+    }
+
+    collide(state) {
+        let filtered = state.actors.filter(a => a !== this);
+        let status = state.status;
+        if (!filtered.some(a => a.type === "coin")) status = "won";
+        return new State(state.level, filtered, status);
+    }
+
+    update(time) {
+        let wobble = this.wobble + time * WOBBLE_SPEED;
+        let wobblePos = Math.sin(wobble) * WOBBLE_DIST;
+        return new Coin(this.basePos.plus(new Vec(0, wobblePos)),
+            this.basePos, wobble);
+    }
 };
-
 Coin.prototype.size = new Vec(0.6, 0.6);
 
+
+let Monster = class Monster {
+    constructor(pos, speed) {
+        this.pos = pos;
+        this.speed = speed;
+    }
+
+    get type() { return "monster"; }
+
+    static create(pos) {
+        return new Monster(pos.plus(new Vec(0, -1)), new Vec(2.5, 0));
+    }
+
+    update(time, state) {
+        let newPos = this.pos.plus(this.speed.times(time));
+        if (!state.level.touches(newPos, this.size, "wall")) {
+            return new Monster(newPos, this.speed);
+        } else {
+            return new Monster(this.pos, this.speed.times(-1));
+        }
+    }
+
+    collide(state) {
+        function jumpedOn(player, monster) {
+            console.log(`playerSpeed=${player.speed.y}`);
+            return player.pos.y < monster.pos.y
+                && player.speed.y > 0;
+        }
+        let actors = state.actors;
+        let status = state.status;
+        if (jumpedOn(state.player, this)) {
+            actors = state.actors.filter(a => a !== this);
+        } else {
+            status = "lost";
+        }
+        return new State(state.level, actors, status);
+    }
+};
+Monster.prototype.size = new Vec(1.2, 2);
+
+
 let levelChars = {
-  ".": "empty", "#": "wall", "+": "lava",
-  "@": Player, "o": Coin,
-  "=": Lava, "|": Lava, "v": Lava
+    ".": "empty", "#": "wall", "+": "lava",
+    "@": Player, "o": Coin,
+    "=": Lava, "|": Lava, "v": Lava,
+    "M": Monster
 };
 
-const simpleLevel = new Level(simpleLevelPlan);
+let simpleLevel = new Level(simpleLevelPlan);
 
 function elt(name, attrs, ...children) {
   let dom = document.createElement(name);
@@ -141,14 +293,12 @@ let DOMDisplay = class DOMDisplay {
   clear() { this.dom.remove(); }
 };
 
-const scale = 20;
-
 function drawGrid(level) {
   return elt("table", {
     class: "background",
-    style: `width: ${level.width * scale}px`
+    style: `width: ${level.width * SCALE}px`
   }, ...level.rows.map(row =>
-    elt("tr", {style: `height: ${scale}px`},
+    elt("tr", {style: `height: ${SCALE}px`},
         ...row.map(type => elt("td", {class: type})))
   ));
 }
@@ -156,10 +306,10 @@ function drawGrid(level) {
 function drawActors(actors) {
   return elt("div", {}, ...actors.map(actor => {
     let rect = elt("div", {class: `actor ${actor.type}`});
-    rect.style.width = `${actor.size.x * scale}px`;
-    rect.style.height = `${actor.size.y * scale}px`;
-    rect.style.left = `${actor.pos.x * scale}px`;
-    rect.style.top = `${actor.pos.y * scale}px`;
+    rect.style.width = `${actor.size.x * SCALE}px`;
+    rect.style.height = `${actor.size.y * SCALE}px`;
+    rect.style.left = `${actor.pos.x * SCALE}px`;
+    rect.style.top = `${actor.pos.y * SCALE}px`;
     return rect;
   }));
 }
@@ -183,7 +333,7 @@ DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
 
   let player = state.player;
   let center = player.pos.plus(player.size.times(0.5))
-                         .times(scale);
+                         .times(SCALE);
 
   if (center.x < left + margin) {
     this.dom.scrollLeft = center.x - margin;
@@ -195,107 +345,6 @@ DOMDisplay.prototype.scrollPlayerIntoView = function(state) {
   } else if (center.y > bottom - margin) {
     this.dom.scrollTop = center.y + margin - height;
   }
-};
-
-Level.prototype.touches = function(pos, size, type) {
-  let xStart = Math.floor(pos.x);
-  let xEnd = Math.ceil(pos.x + size.x);
-  let yStart = Math.floor(pos.y);
-  let yEnd = Math.ceil(pos.y + size.y);
-
-  for (let y = yStart; y < yEnd; y++) {
-    for (let x = xStart; x < xEnd; x++) {
-      let isOutside = x < 0 || x >= this.width ||
-                      y < 0 || y >= this.height;
-      let here = isOutside ? "wall" : this.rows[y][x];
-      if (here === type) return true;
-    }
-  }
-  return false;
-};
-
-State.prototype.update = function(time, keys) {
-  let actors = this.actors
-    .map(actor => actor.update(time, this, keys));
-  let newState = new State(this.level, actors, this.status);
-
-  if (newState.status !== "playing") return newState;
-
-  let player = newState.player;
-  if (this.level.touches(player.pos, player.size, "lava")) {
-    return new State(this.level, actors, "lost");
-  }
-
-  for (let actor of actors) {
-    if (actor !== player && overlap(actor, player)) {
-      newState = actor.collide(newState);
-    }
-  }
-  return newState;
-};
-
-function overlap(actor1, actor2) {
-  return actor1.pos.x + actor1.size.x > actor2.pos.x &&
-         actor1.pos.x < actor2.pos.x + actor2.size.x &&
-         actor1.pos.y + actor1.size.y > actor2.pos.y &&
-         actor1.pos.y < actor2.pos.y + actor2.size.y;
-}
-
-Lava.prototype.collide = function(state) {
-  return new State(state.level, state.actors, "lost");
-};
-
-Coin.prototype.collide = function(state) {
-  let filtered = state.actors.filter(a => a !== this);
-  let status = state.status;
-  if (!filtered.some(a => a.type === "coin")) status = "won";
-  return new State(state.level, filtered, status);
-};
-
-Lava.prototype.update = function(time, state) {
-  let newPos = this.pos.plus(this.speed.times(time));
-  if (!state.level.touches(newPos, this.size, "wall")) {
-    return new Lava(newPos, this.speed, this.reset);
-  } else if (this.reset) {
-    return new Lava(this.reset, this.speed, this.reset);
-  } else {
-    return new Lava(this.pos, this.speed.times(-1));
-  }
-};
-
-let wobbleSpeed = 8, wobbleDist = 0.07;
-
-Coin.prototype.update = function(time) {
-  let wobble = this.wobble + time * wobbleSpeed;
-  let wobblePos = Math.sin(wobble) * wobbleDist;
-  return new Coin(this.basePos.plus(new Vec(0, wobblePos)),
-                  this.basePos, wobble);
-};
-
-let playerXSpeed = 7;
-let gravity = 30;
-let jumpSpeed = 17;
-
-Player.prototype.update = function(time, state, keys) {
-  let xSpeed = 0;
-  if (keys.ArrowLeft) xSpeed -= playerXSpeed;
-  if (keys.ArrowRight) xSpeed += playerXSpeed;
-  let pos = this.pos;
-  let movedX = pos.plus(new Vec(xSpeed * time, 0));
-  if (!state.level.touches(movedX, this.size, "wall")) {
-    pos = movedX;
-  }
-
-  let ySpeed = this.speed.y + time * gravity;
-  let movedY = pos.plus(new Vec(0, ySpeed * time));
-  if (!state.level.touches(movedY, this.size, "wall")) {
-    pos = movedY;
-  } else if (keys.ArrowUp && ySpeed > 0) {
-    ySpeed = -jumpSpeed;
-  } else {
-    ySpeed = 0;
-  }
-  return new Player(pos, new Vec(xSpeed, ySpeed));
 };
 
 function trackKeys(keys) {
@@ -364,8 +413,6 @@ function runLevel(level, Display) {
         });
     });
 }
-
-const INIT_LIVES = 3;
 
 async function runGame(plans, Display) {
     let lives = INIT_LIVES;
